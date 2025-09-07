ecf3d463dc2b8571a1f36d7b6980ce748865240e
Jeremie Drouet <jeremie.drouet@gmail.com>
2025-09-07 21:42:12 +0200

    fix: remove unused traits
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,3 @@
 pub trait Database {
     const SYSTEM: &'static str;
 }
-
-pub trait DatabaseInstance {
-    fn database_name(&self) -> impl std::fmt::Display;
-}
--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,12 +1,3 @@
 impl crate::prelude::Database for sqlx::Sqlite {
     const SYSTEM: &'static str = "sqlite";
 }
-
-impl crate::prelude::DatabaseInstance for &'_ sqlx::Pool<sqlx::Sqlite> {
-    fn database_name(&self) -> impl std::fmt::Display {
-        self.connect_options()
-            .get_filename()
-            .to_string_lossy()
-            .to_string()
-    }
-}
